c85ec0c217ba4a957ee8d939e837630fcd1aa4f5
Eduardo Dominguez Librado <eduardodominguez539@gmail.com>
2025-02-13 12:13:29 -0600

    Modificacion del pdf

--- src/Modelo/GenerarBoleto.java
+++ src/Modelo/GenerarBoleto.java
@@ -14,7 +14,9 @@ import javax.swing.JOptionPane;
 import java.text.ParseException;
 
 public class GenerarBoleto {
-
+    
+    NombreBoleto nomBol = NombreBoleto.getInstancia();
+    
     // Función para obtener el nombre del día de la semana
     private String obtenerDiaSemana(String fecha) {
         try {
@@ -62,8 +64,8 @@ public class GenerarBoleto {
         }
 
         // Obtener el folio y nombre del primer boleto para el nombre del archivo
-        DataPDF primerBoleto = listaBoletos.get(0);
-        String fileName = "BoletosPDF/" + primerBoleto.getFolio() + "_" + primerBoleto.getNombre().replace(" ", "_") + ".pdf";
+        DataPDF BoletoPDF = listaBoletos.get(0);
+        String fileName = "BoletosPDF/" + BoletoPDF.getFolio() + "_" + BoletoPDF.getNumSocio() + "_" + BoletoPDF.getNombre().replace(" ", "_") + ".pdf";
 
         // Crear la carpeta si no existe
         File folder = new File("BoletosPDF");
@@ -116,40 +118,63 @@ public class GenerarBoleto {
                 
                 // ----------FECHA-------------//
                 cb.setFontAndSize(bf, 8);
-                cb.setTextMatrix(175, 85);
-                // Convertir el texto a mayúsculas
+                //String vchDay = diaSemanaVigencia;
+                float dayWidth = bf.getWidthPoint(diaSemanaVigencia, 8);
+                float centerday = (375 - dayWidth) / 2;
+                cb.setTextMatrix(centerday, 85);
                 cb.showText(diaSemanaVigencia.toUpperCase());//Dia de Vigencia(Palabra del dia)
                 
                 cb.setFontAndSize(bf, 25);
-                cb.setTextMatrix(179, 65);
-                cb.showText(diaNumeroVigencia);//Dia de vigencia
+                float textWidth = bf.getWidthPoint(diaNumeroVigencia, 25); // Obtener el ancho del texto
+                float centerX = (385 - textWidth) / 2; // Centrar en una anchura de 385 pt
+                cb.setTextMatrix(centerX, 65); // Usar la nueva posición centrada
+                cb.showText(diaNumeroVigencia.toUpperCase());
                 
                 cb.setFontAndSize(bf, 7);
-                cb.setTextMatrix(174, 55);
-                cb.showText(mesVigencia.toUpperCase());//Mes de la vigencia
+                float mesWidth = bf.getWidthPoint(mesVigencia, 7);
+                float centerMesa = (375 - mesWidth) / 2;
+                cb.setTextMatrix(centerMesa, 55);//174
+                cb.showText(mesVigencia.toUpperCase());
                 //----------------------------//
 
                 
                 // ----------DATOS-------------//
-                cb.setColorFill(new GrayColor(1)); // Color negro
-                cb.setFontAndSize(bf, 5);
+                String nombre1Limite = dataPdf.getNombre().length() > 31 ? dataPdf.getNombre().substring(0, 31) : dataPdf.getNombre();
+                cb.setColorFill(new GrayColor(1));// Color negro
+                cb.setFontAndSize(bf, 6);
                 cb.setTextMatrix(50, 47);
-                cb.showText(dataPdf.getNombre());
-                
-                cb.setTextMatrix(75, 34);
-                cb.showText("" + dataPdf.getNumSocio());
-                
-                cb.setTextMatrix(75, 22);
-                cb.showText(dataPdf.getSucursal());
-                
-                cb.setTextMatrix(75, 8);
-                cb.showText(dataPdf.getInvitado());
-                //----------------------------//
+                cb.showText(nombre1Limite);//Nombre
 
-                // ----------FOLIO-------------//
-                cb.setColorFill(new CMYKColor(1, 1, 1, 1)); // Color blanco
-                cb.setFontAndSize(bf, 8);
-                cb.setTextMatrix(187, 9);
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(50, 29);
+                cb.showText(dataPdf.getSucursal());//Sucursal
+
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(150, 29);
+                cb.showText(dataPdf.getInvitado());//Invitado
+
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(50, 10);
+                cb.showText("" + dataPdf.getNumSocio());//Socio
+
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(83, 10);
+                cb.showText(dataPdf.getZona());//Zona
+
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(104, 10);
+                cb.showText(dataPdf.getDescMesa());//Mesa
+
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(134, 10);
+                cb.showText(dataPdf.getVchDescripcion());//Silla
+                //----------------------------//
+                
+                
+                //----------FOLIO-------------//
+                cb.setColorFill(new CMYKColor(1, 1, 1, 1));// Color blanco
+                cb.setFontAndSize(bf, 7);
+                cb.setTextMatrix(179, 5);
                 cb.showText("" + dataPdf.getFolio());
                 //----------------------------//
                 
@@ -164,29 +189,42 @@ public class GenerarBoleto {
                 cb.showText(nombreLimite);//Nombre
 
                 cb.setFontAndSize(bf, 5);
-                cb.setTextMatrix(318, 83);
+                cb.setTextMatrix(293, 82);
                 cb.showText("" + dataPdf.getNumSocio());//No Socio
+
+                cb.setFontAndSize(bf, 5);
+                cb.setTextMatrix(338, 82);
+                cb.showText(dataPdf.getInvitado());//INVITADO
 
                 cb.setFontAndSize(bf, 5);
                 cb.setTextMatrix(292, 64);
                 cb.showText(dataPdf.getSucursal());//SUCURSAL
 
                 cb.setFontAndSize(bf, 5);
-                cb.setTextMatrix(318, 48);
-                cb.showText(dataPdf.getInvitado());//INVITADO
+                cb.setTextMatrix(292, 47);
+                cb.showText(dataPdf.getTelefono());//TELEFÓNO
 
                 cb.setFontAndSize(bf, 5);
-                cb.setTextMatrix(292, 31);
-                cb.showText("" + dataPdf.getTelefono());//TELEFÓNO
+                cb.setTextMatrix(296, 30);
+                cb.showText(dataPdf.getZona());//Zona
 
-                cb.setFontAndSize(bf, 8);
-                cb.setTextMatrix(318, 14);
+                cb.setFontAndSize(bf, 5);
+                cb.setTextMatrix(316, 30);
+                cb.showText(dataPdf.getDescMesa());//Mesa
+
+                cb.setFontAndSize(bf, 5);
+                cb.setTextMatrix(345, 30);
+                cb.showText(dataPdf.getVchDescripcion());//Silla
+
+                cb.setFontAndSize(bf, 6);
+                cb.setTextMatrix(316, 15);
                 cb.showText("" + dataPdf.getFolio());//Folio
                 //----------------------------------------//
 
                 cb.endText();
-            
             }
+            
+            nomBol.setNomBoleto(fileName);
 
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Ocurrió un error al generar el PDF: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
