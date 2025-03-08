2c0e848ab20c46ca5a53a05f58ca0702056af783
Eduardo20210681LALO <20210681@uthh.edu.mx>
2025-03-08 11:40:39 -0600

    Save local

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -15,19 +15,19 @@ public class Conexion {
     
     // Datos de conexión a la base de datos
     
-    
+    /*
     private static final String URL = "jdbc:mysql://localhost:3306/";
     private static final String DATABASE = "eventos";
     private static final String USER = "root";
     private static final String PASSWORD = "";
+    */
     
     
-    /*
     private static final String URL = "jdbc:mysql://133.145.6.36:3306/";
     private static final String DATABASE = "eventos";
-    private static final String USER = "root";
+    private static final String USER = "eduHdz";
     private static final String PASSWORD = "root";
-    */
+    
     
     // Tiempo de espera para la conexión
     private static final int TIMEOUT_SECONDS = 10;
