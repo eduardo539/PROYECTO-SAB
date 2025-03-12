04bcee8b76c9c8621117245f63225a9527ddf462
Eduardo Dominguez Librado <eduardodominguez539@gmail.com>
2025-03-12 15:16:42 -0600

    Correcciones en el codigo

--- src/Vista/frmReporteVentaPSucursal.java
+++ src/Vista/frmReporteVentaPSucursal.java
@@ -1,6 +1,7 @@
 package Vista;
 
 import FormulariosAyuda.Gerente.AyudaVentasXSucursal;
+import Modelo.CerrarSesion;
 import Modelo.Login;
 import Modelo.Conexion;
 import Modelo.TimeGoogle;
@@ -95,21 +96,7 @@ public class frmReporteVentaPSucursal extends javax.swing.JFrame {
             }
         });
     }
-    
-    private void cerrarSesion() {
-        // Si tienes una clase Singleton para manejar la sesión
-        Login sesion = Login.getInstancia();
-        sesion.limpiarDatos();
 
-        // Si la clase no implementa un método limpiarDatos(), puedes hacer:
-        sesion.setIdusuario(0);
-        sesion.setNombre(null);
-        sesion.setSucursal(null);
-        sesion.setVigencia(null);
-        sesion.setIdperfil(0);
-        sesion.setTipo_perfil(null);
-
-    }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -396,7 +383,11 @@ public class frmReporteVentaPSucursal extends javax.swing.JFrame {
 
             if (confirm == JOptionPane.YES_OPTION) {
                 // Limpiar datos de la sesión del usuario
-                cerrarSesion();
+                CerrarSesion closeSesion = new CerrarSesion();
+                int user = lg.getIdusuario();
+                
+                closeSesion.EliminarSesion(user);
+                closeSesion.cerrarSession();
 
                 // Cerrar todas las ventanas abiertas excepto el login
                 JFrame topFrame = (JFrame) javax.swing.SwingUtilities.getWindowAncestor(this);
