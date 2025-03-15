4fe38c4b11de519e481f9e4ef84810b4142e4670
Eduardo Dominguez Librado <eduardodominguez539@gmail.com>
2025-03-15 12:01:12 -0600

    Correcciones en algunos formularios

--- src/Notas/Notas.java
+++ src/Notas/Notas.java
@@ -7,6 +7,18 @@ package Notas;
 public class Notas {
     
     /**
+       git config --global user.name "Tu Nombre"
+       git config --global user.email "tu_correo@example.com"
+
+*      revisar las ramas existentes
+*      git branch
+* 
+*      conectar a una rama ya existente
+*      git checkout "tu rama"
+* 
+*      Para crear una nueva rama y cambiar a ella, usa:
+*      git checkout -b nueva-rama
+
      
        git reset --hard origin/nombre-de-tu-rama
        git fetch origin descargará los cambios del repositorio remoto.
